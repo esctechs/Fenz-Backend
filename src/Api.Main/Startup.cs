ec3d4e76e00acd02f0eee84b702f06151fa87c8b
evertonesc <evertonsouzaesc@gmail.com>
2021-09-24 21:23:12 -0300

    refactor: Removing swagger configuration from standart .NET development condition

--- src/Api.Main/Startup.cs
+++ src/Api.Main/Startup.cs
@@ -37,12 +37,13 @@ namespace Api.Main
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //Remove this condition when using Sentry
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api.Main v1"));
             }
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api.Main v1"));
 
             app.UseHttpsRedirection();
 
