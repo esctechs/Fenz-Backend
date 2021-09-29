e240cc284109092760e7348698cafa66c4231d07
evertonesc <evertonsouzaesc@gmail.com>
2021-09-29 16:33:06 -0300

    Add docker file to Api.Main project

---
+++ src/Api.Main/Config/Patterns/IOptionsPattern.cs
@@ -0,0 +1,12 @@
+﻿using Api.Main.Middleware;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Api.Main.Config.Patterns
+{
+    public static class IOptionsPattern
+    {
+        public static IServiceCollection AddIOptionsPattern(this IServiceCollection services, IConfiguration config) =>
+            services.Configure<HttpException>(config.GetSection("HttpException"));
+    }
+}
