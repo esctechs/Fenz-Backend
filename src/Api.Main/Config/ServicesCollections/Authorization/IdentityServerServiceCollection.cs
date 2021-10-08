59bb5d95f5c581e228e3139a607e1498106efbf1
evertonesc <evertonsouzaesc@gmail.com>
2021-10-08 18:04:48 -0300

    chore: Adding IdentityServer

---
+++ src/Api.Main/Config/ServicesCollections/Authorization/IdentityServerServiceCollection.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer4.AccessTokenValidation;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+namespace Api.Main.Config.ServicesCollections.Authorization
+{
+    public static class IdentityServerServiceCollection
+    {
+        public static IServiceCollection AddIdentityServer4(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(opts =>
+                {
+                    opts.JwtBackChannelHandler = new HttpClientHandler
+                    {
+                        ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                    };
+                    opts.RequireHttpsMetadata = false;
+                    opts.Authority = configuration["IdentityServer4:Authority"];
+                    opts.ApiName = configuration["IdentityServer4:Audience"];
+                });
+
+            services.AddAuthorization(opts =>
+            {
+                opts.AddPolicy("fenz-services", builder =>
+                {
+                    builder.RequireScope("fenz.api");
+                });
+            });
+
+            return services;
+        }
+    }
+}
--- src/Api.Main/Startup.cs
+++ src/Api.Main/Startup.cs
@@ -1,7 +1,7 @@
 using Api.Main.Config;
 using Api.Main.Config.Patterns;
+using Api.Main.Config.ServicesCollections.Authorization;
 using Api.Main.Middleware;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,7 +21,6 @@ namespace Api.Main
         public IConfiguration Configuration { get; }
         public IWebHostEnvironment WebHostEnvironment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -31,31 +30,20 @@ namespace Api.Main
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api.Main", Version = "v1" });
             });
 
-            //services.AddAuthorization(opts =>
-            //{
-            //    opts.AddPolicy("fenz-services", builder =>
-            //    {
-            //        builder.RequireScope("fenz-api");
-            //    });
-            //});
-
-            //Temporary services registration
+            services.AddIdentityServer4(Configuration);
             services.LoadJsonFiles(WebHostEnvironment);
             services.AddIOptionsPattern(Configuration);
 
+            //Temporary Registration
             services.AddSingleton<IRequestHandler, RequestHandler>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //Remove this condition when using Sentry
-
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api.Main v1"));
 
             app.AddMiddleware();
-
 
             app.UseHttpsRedirection();
 
